c52d32bdf254b097456f135c1eb2f419cfdefe3c
Daniel Lim <cloud9@profoundideation.com>
2016-11-15 06:08:01 +0000

    last update of the day, hope it does not break anything

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
-module: {
-   loaders: [
-    { test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    },
-    { test: /\.css$/, loader: "style!css" },
-    { test: /\.(png|jpg|jpeg|gif|woff)$/, loader: 'url?limit=8192' },
-    { test: /\.(otf|eot|ttf)$/, loader: "file?prefix=font/" },
-    { test: /\.svg$/, loader: "file" }
+var path = require('path');
+
+var port = process.env.PORT || 8080;
+var host = process.env.IP || '127.0.0.1';
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'normalize.css',
+    './client/css/main.css',
+    'eventsource-polyfill' // necessary for hot reloading with IE
   ],
-}
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      include: path.join(__dirname, 'src'),
+      query: {
+        presets: [ 'es2015', 'react', 'react-hmre' ]
+      }
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+    }]
+  },
+  resolve: {
+    root: path.resolve('./client'),
+  },
+  devServer: {
+    port: port,
+    host: host
+  }
+};
