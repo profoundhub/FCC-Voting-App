c96adb1f6d3108ddedef02791a49f3648c665762
slfweb <slf.pro@gmail.com>
2016-11-16 15:27:54 +0100

    comments added in server.js #2

--- server.js
+++ server.js
@@ -8,23 +8,25 @@ var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/client', express.static(process.cwd() + '/client'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/client', express.static(process.cwd() + '/client'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
 
-
+// there is really only one session, which is managed by Express.
+// Passport merely piggy backs off the ExpressJS session to store data for authenticated users.
 app.use(session({
 	secret: 'secretClementine4VotingApp390',
 	resave: false,
 	saveUninitialized: true
 }));
 
+// initialize session after static files.
+// cf: https://www.airpair.com/express/posts/expressjs-and-passportjs-sessions-deep-dive, 'Avoid Sessions for Static Resources' section.
 app.use(passport.initialize());
 app.use(passport.session());
 
