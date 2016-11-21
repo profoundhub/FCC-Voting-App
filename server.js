0c748f199f659f6f3a9e9ff460b56d7cb9351137
Daniel Lim <hbt@markhamboards.com>
2016-11-21 12:39:54 -0500

    webpack now works
    webpack now works

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var passport = require('passport');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var bodyParser = require('body-parser');
-var morgan = require('morgan');
 
 var app = express();
 require('dotenv').load();
