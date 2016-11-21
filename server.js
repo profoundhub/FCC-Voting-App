d30861f79f97c01e5a39afcc20200168a1a7568c
Daniel Lim <hbt@markhamboards.com>
2016-11-21 13:34:58 -0500

    update Daniel Branch
    update Daniel Branch

--- server.js
+++ server.js
@@ -41,17 +41,11 @@ app.use(session({
 	resave: false,
 	saveUninitialized: true,
 	name: 'sessionId',
-	// using store session on MongoDB using express-session + connect
-	// 	store: new MongoStore({
-	// 		url: config.mongoURI[process.env.NODE_ENV],
-	// 	collection: 'sessions'
-	// }),
-	sessionStore : new MongoStore({
-        host: '127.0.0.1',
-        port: '27017',
-        db: 'session',
-        url: 'mongodb://localhost:27017/vote'
-    })
+	//using store session on MongoDB using express-session + connect
+	store: new MongoStore({
+		url: config.mongoURI[process.env.NODE_ENV],
+		collection: 'sessions'
+	})
 }));
 
 app.use(passport.initialize());
