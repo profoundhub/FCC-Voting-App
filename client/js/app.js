bc2471470c8459f6fa98cb49d131b20759cb9c12
Daniel Lim <cloud9@profoundideation.com>
2016-11-21 17:25:05 +0000

    update cloud version

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
+// import App from './components/App';
+// import Main from './components/Main';
 
 // Render the main component into the dom
-var rootId = document.getElementById('app');
-ReactDOM.render(<App />, rootId);
+const rootId = document.getElementById('main');
+
+// ReactDOM.render(<App />, rootId);
+ReactDOM.render(<p>Hello App User</p>, rootId);
