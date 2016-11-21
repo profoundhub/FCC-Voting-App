da52d9b944d4b69a4ca7ef0972f552dc7b8b8575
Daniel Lim <cloud9@profoundideation.com>
2016-11-21 17:04:14 +0000

    update Front End  Setup Files

--- client/js/app.js
+++ client/js/app.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 // Render the main component into the dom
-var rootEl = document.getElementById('app');
-ReactDOM.render(<App />, rootEl);
+var rootId = document.getElementById('app');
+ReactDOM.render(<App />, rootId);
