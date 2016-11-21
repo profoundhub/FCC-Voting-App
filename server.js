56798fea1c75526b79cf69f547d77604b539f06e
Daniel Lim <hbt@markhamboards.com>
2016-11-21 15:00:00 -0500

    update ===

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var passport = require('passport');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var app = express();
 require('dotenv').load();
