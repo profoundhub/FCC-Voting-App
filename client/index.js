bb03d332f5923380bbb232bc6da6ac165466408b
Daniel Lim <cloud9@profoundideation.com>
2016-11-22 01:44:43 +0000

    fix React #2

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import App from './components/main';
 
 // Render the main component into the dom
-var rootEl = document.getElementById('app');
-ReactDOM.render(<App />, rootEl);
+const rootId = document.getElementById('app');
+render(<App />, rootId);
