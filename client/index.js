b49fa703ecd07ee2d56033dcc5e1783bb3d61218
Daniel Lim <cloud9@profoundideation.com>
2016-11-22 03:20:30 +0000

    update current State # 2

--- client/index.js
+++ client/index.js
@@ -8,16 +8,17 @@ import QuizGrid from './components/QuizGrid';
 import Footer   from './components/Footer';
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import store, { history } from './client/store';
+import storage, { history } from './storage';
 
 const router = (
     
-    <Router history={ browserHistory }>
+    <Router history={ history }>
         <Route path="/client" component={ Main }>
             <IndexRoute component={ QuizGrid }></IndexRoute>
             <Route path="/view/:postId" component={ Single }></Route>
         </Route>    
     </Router>
+    
 )
     
 // Render the main component into the dom
---
+++ data/comments.js
@@ -0,0 +1,154 @@
+const comments = {
+  "eVARmz9SrL":[
+    {
+      "text":"1",
+      "user": "2"
+    }
+  ],
+  "FbFfkVqXsD":[
+    {
+      "text":"1",
+      "user": "2"
+    },
+    {
+      "text": "3",
+      "user": "3"
+    },
+    {
+      "text":"4",
+      "user": "5"
+    },
+    {
+      "text":"6",
+      "user": "7"
+    }
+  ],
+  "YSuBNJlVRc":[
+    {
+      "text": "11",
+      "user": "2"
+    },
+    {
+      "text": "13",
+      "user": "3"
+    },
+    {
+      "text":"14",
+      "user": "5"
+    },
+    {
+      "text":"16",
+      "user": "7"
+    }
+  ],
+  "9jFTeunbTv":[
+   {
+      "text":"12",
+      "user": "2"
+    },
+    {
+      "text": "23",
+      "user": "3"
+    },
+    {
+      "text":"42",
+      "user": "5"
+    },
+    {
+      "text":"26",
+      "user": "7"
+    }
+  ],
+  "DUHlFLRq3p":[
+    {
+      "text":"31",
+      "user": "2"
+    },
+    {
+      "text": "33",
+      "user": "3"
+    },
+    {
+      "text":"34",
+      "user": "5"
+    },
+    {
+      "text":"63",
+      "user": "7"
+    }
+  ],
+  "8yKl3cKtiW":[
+    {
+      "text": "51",
+      "user": "2"
+    },
+    {
+      "text": "3",
+      "user": "53"
+    },
+    {
+      "text":"45",
+      "user": "5"
+    },
+    {
+      "text":"6",
+      "user": "57"
+    }
+  ],
+  "nhSoPLoZKm":[
+    {
+      "text": "1",
+      "user": "62"
+    },
+    {
+      "text": "3",
+      "user": "63"
+    },
+    {
+      "text":"64",
+      "user": "65"
+    },
+    {
+      "text":"66",
+      "user": "67"
+    }
+  ],
+  "OM8nGkgdde":[
+    {
+      "text":"71",
+      "user": "2"
+    },
+    {
+      "text": "73",
+      "user": "3"
+    },
+    {
+      "text":"74",
+      "user": "5"
+    },
+    {
+      "text":"76",
+      "user": "77"
+    }
+  ],
+  "uoaXsPIdKs":[
+    {
+      "text":"18",
+      "user": "2"
+    },
+    {
+      "text": "83",
+      "user": "3"
+    },
+    {
+      "text":"84",
+      "user": "85"
+    },
+    {
+      "text":"6",
+      "user": "87"
+    }
+  ]
+};
+
+export default comments;
