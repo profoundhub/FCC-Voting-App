f64d9622b4fed6ee42c0b3cc203e0838ce85b2d7
Daniel Lim <cloud9@profoundideation.com>
2016-11-28 18:01:15 +0000

    update on Cloud Branch

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,8 @@ module.exports = {
         presets: [ 'es2015', 'react', 'react-hmre' ]
       }
     }, {
-      test: /\.css$/,
-      loaders: ['style', 'css'],
+      test: /\.css$/, 
+      loader: "style-loader!css-loader"
     }]
   },
   resolve: {
